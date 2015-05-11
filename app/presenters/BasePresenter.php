b81219f9c62dd93ae49c370677fd70180349bd1c
Josef Kolář <thejoeejoee@gmail.com>
2015-05-11 20:19:57 +0200

    moved templates

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -28,4 +28,38 @@ class BasePresenter extends Presenter {
         $this->redrawControl('flashes');
     }
 
+    /**
+     * Formats layout template file names.
+     * @return array
+     */
+    public function formatLayoutTemplateFiles() {
+        $layout = $this->layout ? $this->layout : 'layout';
+        $dir = $this->context->parameters['appDir'];
+        $presenter = $this->getName();
+        $dir = is_dir("$dir/templates") ? $dir : dirname($dir);
+        $list = array(
+            "$dir/templates/$presenter/@$layout.latte",
+            "$dir/templates/$presenter.@$layout.latte",
+            "$dir/templates/@$layout.latte",
+        );
+        return $list;
+    }
+
+
+    /**
+     * Formats view template file names.
+     * @return array
+     */
+    public function formatTemplateFiles() {
+        $dir = $this->context->parameters['appDir'];
+        $presenter = $this->getName();
+        $dir = is_dir("$dir/templates") ? $dir : dirname($dir);
+        $list = array(
+            "$dir/templates/$presenter.$this->view.latte",
+            "$dir/templates/$presenter.$this->view.latte",
+            "$dir/templates/$presenter/$this->view.latte",
+        );
+        return $list;
+    }
+
 }
