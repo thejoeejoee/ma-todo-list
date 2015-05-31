ab6b4fae4461850b25aeee05d8dff57b2ee67a09
Josef Kolář <thejoeejoee@gmail.com>
2015-05-31 21:30:30 +0200

    urls

--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -5,4 +5,10 @@ namespace App\Presenters;
 
 class SignPresenter extends BasePresenter {
 
+    protected function startup() {
+        parent::startup();
+        if ($this->user->loggedIn) {
+            $this->redirect('Homepage:default');
+        }
+    }
 }
--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -2,22 +2,21 @@
 
 namespace App;
 
-use Nette,
-	Nette\Application\Routers\RouteList,
-	Nette\Application\Routers\Route;
+use Nette;
+use Nette\Application\Routers\Route;
+use Nette\Application\Routers\RouteList;
 
 
-class RouterFactory
-{
+class RouterFactory {
 
-	/**
-	 * @return Nette\Application\IRouter
-	 */
-	public static function createRouter()
-	{
-		$router = new RouteList();
-		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
-		return $router;
-	}
+    /**
+     * @return Nette\Application\IRouter
+     */
+    public static function createRouter() {
+        $router = new RouteList();
+        $router[] = new Route('login', 'Sign:in');
+        $router[] = new Route('<presenter>/<action>', 'Homepage:default');
+        return $router;
+    }
 
 }
